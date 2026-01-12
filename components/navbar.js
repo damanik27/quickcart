b1cdc9f5cf2768cb392881aaf90522b76f4c88ef
raska26 <radityasandika51@gmail.com>
2026-01-13 06:36:56 +0700

    Menambahkan tombol Account pada Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -42,6 +42,10 @@ const Navbar = () => {
         </button>
         </ul>
               <div className="flex items-center md:hidden gap-3">
+                 <button className="flex items-center gap-2 hover:text-gray-900 transition">
+          <Image src={assets.user_icon} alt="user icon" />
+          Account
+        </button>
                 </div>
 
       </nav>
