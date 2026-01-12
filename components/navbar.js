928eac7f339ef20bcca35b270d2e877dd4d9eef2
raska26 <radityasandika51@gmail.com>
2026-01-13 06:31:56 +0700

    Menambahkan search icon di Navbar

--- components/navbar.js
+++ components/navbar.js
@@ -35,6 +35,7 @@ const Navbar = () => {
           </div>
 
            <ul className="hidden md:flex items-center gap-4 "></ul>
+            <Image className="w-4 h-4" src={assets.search_icon} alt="search icon" />
       </nav>
     );
   }
