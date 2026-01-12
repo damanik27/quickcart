358b36ef8aa20119a5bee0a9e9ec24b35b0f3456
raska26 <radityasandika51@gmail.com>
2026-01-13 06:18:34 +0700

    Menambahkan logo navbar dengan navigasi ke halaman utama

--- components/navbar.js
+++ components/navbar.js
@@ -9,4 +9,18 @@ const Navbar = () => {
 
   const { isSeller, router } = useAppContext();
 
+    return (
+      <nav className="flex items-center justify-between px-6 md:px-16 lg:px-32 py-3 border-b border-gray-300 text-gray-700">
+        <Image
+          className="cursor-pointer w-28 md:w-32"
+          onClick={() => router.push('/')}
+          src={assets.logo}
+          alt="logo"
+        />
+      </nav>
+    );
+  }
+  
+  export default Navbar;
+
 
