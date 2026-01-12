0cf798594162f3cf4c261b2fa7ab71dedd339e2f
raska26 <radityasandika51@gmail.com>
2026-01-13 06:05:45 +0700

    Menambahkan logo dan navigasi ke halaman berand

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,6 @@
 "use client"
+import React from "react";
+import { assets } from "@/assets/assets";
+import Link from "next/link";
+import { useAppContext } from "@/context/AppContext";
+import Image from "next/image";
