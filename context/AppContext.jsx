ba24d7f008c7153cf137ba18fa04cbfc173d7540
damanik27 <wilsondamanik27@gmail.com>
2026-01-13 10:04:11 +0700

    export const AppContextProvide

--- context/AppContext.jsx
+++ context/AppContext.jsx
@@ -6,3 +6,26 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
+
+export const AppContextProvider = (props) => {
+
+    const currency = process.env.NEXT_PUBLIC_CURRENCY
+    const router = useRouter()
+
+    const { user } = useUser()
+
+    const [products, setProducts] = useState([])
+    const [userData, setUserData] = useState(false)
+    const [isSeller, setIsSeller] = useState(true)
+    const [cartItems, setCartItems] = useState({})
+
+    const fetchProductData = async () => {
+        setProducts(productsDummyData)
+    }
+
+    const fetchUserData = async () => {
+        setUserData(userDummyData)
+    }
