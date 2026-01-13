b4e477b6f860850d72cfdd15ea7689927469dcbe
raska26 <radityasandika51@gmail.com>
2026-01-13 14:28:17 +0700

    Menambahkan sinkronisasi user Clerk saat user dibuat

--- config/inngest.js
+++ config/inngest.js
@@ -1,3 +1,19 @@
 import {Inngest} from "inngest";
 
 export const inngest = new Innges({id: "app/config/inngest" });
+
+export const syncUserEvent = inngest.createFunction(
+   { 
+    id: "clerk/sync-created" 
+  },
+  { 
+    event: "clerk/user.created" 
+  },
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      profile_image_url,
+    } = event.data;
