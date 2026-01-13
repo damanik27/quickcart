2614295eb68bd499f4b8b2aaa52e4556946d2baf
rofiivor <masterrofi53995@gmail.com>
2026-01-13 17:21:05 +0700

    fix: add user id to synced user data

--- config/inngest.js
+++ config/inngest.js
@@ -11,6 +11,7 @@ export const syncUserEvent = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
+      _id: id,
       email: email_addresses?.[0]?.email_address,
       name: `${first_name} ${last_name}`,
       imageUrl: profile_image_url,
