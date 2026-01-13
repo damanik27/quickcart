64be2408e719af73dc6b24d132c083a2b2ec8304
damanik27 <wilsondamanik27@gmail.com>
2026-01-13 09:40:38 +0700

    use client

---
+++ context/AppContext.jsx
@@ -0,0 +1,12 @@
+'use client'
+import { productsDummyData, userDummyData } from "@/assets/assets";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect, useState } from "react";
+
+export const AppContext = createContext();
+
+export const useAppContext = () => {
+    return useContext(AppContext)
+}
+
+export const AppContextProvider = (props) => {
