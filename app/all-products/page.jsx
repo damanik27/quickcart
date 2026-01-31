0e98dc6c961c41f836f23918aae7158726edd530
rofiivor <masterrofi53995@gmail.com>
2026-02-01 03:19:30 +0700

    buat komponen allproducts

--- app/all-products/page.jsx
+++ app/all-products/page.jsx
@@ -3,3 +3,5 @@ import ProductCard from "@/components/ProductCard";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useAppContext } from "@/context/AppContext";
+
+const AllProducts = () => {};
