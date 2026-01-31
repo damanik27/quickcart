04745f69fa558b5b47c08345cbe1bbe6926b4b50
rofiivor <masterrofi53995@gmail.com>
2026-02-01 04:39:18 +0700

    Render list orders

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -28,7 +28,15 @@ const MyOrders = () => {
       <div className="flex flex-col justify-between px-6 md:px-16 lg:px-32 py-6 min-h-screen">
          <div className="space-y-5">
            <h2 className="text-lg font-medium mt-6">My Orders</h2>
-           {loading ? <Loading />
+           {loading ? <Loading /> : (<div className="max-w-5xl border-t border-gray-300 text-sm">
+              {orders.map((order, index) => (
+                  <div key={index} className="flex flex-col md:flex-row gap-5 justify-between p-5 border-b border-gray-300">
+                      <div className="flex-1 flex gap-5 max-w-80">
+                          <Image
+                              className="max-w-16 max-h-16 object-cover"
+                              src={assets.box_icon}
+                              alt="box_icon"
+                          />
       </div>
       <Footer />
     </>
