eda040c8b5ed5acbc3e3765242978bb423860e13
rofiivor <masterrofi53995@gmail.com>
2026-02-01 03:50:07 +0700

    Header cart

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -19,7 +19,18 @@ const Cart = () => {
   return (
     <>
       <Navbar />
-      <div className="flex flex-col md:flex-row gap-10 px-6 md:px-16 lg:px-32 pt-14 mb-20"></div>
+      <div className="flex flex-col md:flex-row gap-10 px-6 md:px-16 lg:px-32 pt-14 mb-20">
+        <div className="flex-1">
+        <div className="flex items-center justify-between mb-8 border-b border-gray-500/30 pb-6">
+          <p className="text-2xl md:text-3xl text-gray-500">
+            Your <span className="font-medium text-orange-600">Cart</span>
+          </p>
+          <p className="text-lg md:text-xl text-gray-500/80">
+            {getCartCount()} Items
+          </p>
+        </div>
+
+      </div>
     </>
   );
 };
