2167b66a9225557713652f50804fc37bfba73882
rofiivor <masterrofi53995@gmail.com>
2026-02-01 02:40:40 +0700

    input pin code

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -37,6 +37,12 @@ const AddAddress = () => {
                     type="text"
                     placeholder="Phone number"
                 />
+                <input
+                    className="px-2 py-2.5 border border-gray-500/30 rounded outline-none w-full text-gray-500"
+                    type="text"
+                    placeholder="Pin code"
+                />
+
 
 
               </div>
