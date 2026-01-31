bb69b4776c79af1d48daf825abf861cf3dd153de
rofiivor <masterrofi53995@gmail.com>
2026-02-01 02:44:55 +0700

    input state

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -53,6 +53,12 @@ const AddAddress = () => {
                       type="text"
                       placeholder="City/District/Town"
                   />
+                  <input
+                      className="px-2 py-2.5 border border-gray-500/30 rounded outline-none w-full text-gray-500"
+                      type="text"
+                      placeholder="State"
+                  />
+
 
                 </div>
 
