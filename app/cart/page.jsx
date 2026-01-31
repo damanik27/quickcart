31d838e55956455f74e3b05d950a237f9abc51ae
rofiivor <masterrofi53995@gmail.com>
2026-02-01 03:41:52 +0700

    import navbar

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -3,3 +3,4 @@ import React from "react";
 import { assets } from "@/assets/assets";
 import OrderSummary from "@/components/OrderSummary";
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
