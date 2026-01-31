3b4da6ea2901f5aebdb149b8dc289a177c510382
rofiivor <masterrofi53995@gmail.com>
2026-02-01 02:03:22 +0700

    fix add-address page

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -3,3 +3,9 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import { useState } from "react";
+
+const AddAddress = () => {
+  return <div>Add Address Page</div>;
+};
+
+export default AddAddress;
