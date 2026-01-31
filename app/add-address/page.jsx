95710b504f2ea7735dd39242b8e96fe2aa2e67f2
rofiivor <masterrofi53995@gmail.com>
2026-02-01 02:38:45 +0700

    input phonenumber

--- app/add-address/page.jsx
+++ app/add-address/page.jsx
@@ -32,6 +32,12 @@ const AddAddress = () => {
                     type="text"
                     placeholder="Full name"
                 />
+                <input
+                    className="px-2 py-2.5 border border-gray-500/30 rounded outline-none w-full text-gray-500"
+                    type="text"
+                    placeholder="Phone number"
+                />
+
 
               </div>
 
