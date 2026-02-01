54e59a698b85a8ddb61eaeda67bb7a3799103e1c
raska26 <radityasandika51@gmail.com>
2026-02-01 08:56:34 +0700

    menambahkan state dan fetch data orders

--- web/app/seller/orders/page.tsx
+++ web/app/seller/orders/page.tsx
@@ -5,3 +5,15 @@ import Image from "next/image";
 import { useAppContext } from "@/context/AppContext";
 import Footer from "@/components/seller/Footer";
 import Loading from "@/components/Loading";
+
+const Orders = () => {
+
+    const { currency } = useAppContext();
+
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchSellerOrders = async () => {
+        setOrders(orderDummyData);
+        setLoading(false);
+    }
