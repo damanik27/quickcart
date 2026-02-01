fdf20ec5462b389e8aa6075f843877ca79372111
raska26 <radityasandika51@gmail.com>
2026-02-01 08:55:28 +0700

    inisialisasi client component dan import dependencies

---
+++ web/app/seller/orders/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+import React, { useEffect, useState } from "react";
+import { assets, orderDummyData } from "@/assets/assets";
+import Image from "next/image";
+import { useAppContext } from "@/context/AppContext";
+import Footer from "@/components/seller/Footer";
+import Loading from "@/components/Loading";
