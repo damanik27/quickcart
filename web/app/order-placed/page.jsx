94de94787f705463f577b1dbdb6d96533b556e54
raska26 <radityasandika51@gmail.com>
2026-02-01 07:23:19 +0700

    menambahkan indikator loading animasi

--- web/app/order-placed/page.jsx
+++ web/app/order-placed/page.jsx
@@ -19,5 +19,7 @@ return (
   <div className='h-screen flex flex-col justify-center items-center gap-5'>
     <div className="flex justify-center items-center relative"></div>
     <Image className="absolute p-5" src={assets.checkmark} alt='' />
+      <div className="animate-spin rounded-full h-24 w-24 border-4 border-t-green-300 border-gray-200"></div>
+      
 
 
