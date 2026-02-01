1943c20df0cfa7f55f22c6ffbb13f6435c00a352
raska26 <radityasandika51@gmail.com>
2026-02-01 09:16:54 +0700

    tambah ikon kotak untuk setiap order

--- web/app/seller/orders/page.tsx
+++ web/app/seller/orders/page.tsx
@@ -32,6 +32,12 @@ const Orders = () => {
                         <div key={index} className="flex flex-col md:flex-row gap-5 justify-between p-5 border-t border-gray-300">
                             <div className="flex-1 flex gap-5 max-w-80">
 
+                                 <Image
+                                    className="max-w-16 max-h-16 object-cover"
+                                    src={assets.box_icon}
+                                    alt="box_icon"
+                                />
+
                                 
 
 
