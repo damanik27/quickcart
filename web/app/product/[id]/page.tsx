bbe77b12baa637bba781146ad5668f21915539b6
raska26 <radityasandika51@gmail.com>
2026-02-01 07:30:48 +0700

    fitur: menambahkan client component dan dependensi

---
+++ web/app/product/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import { useEffect, useState } from "react";
+import { assets } from "@/assets/assets";
+import ProductCard from "@/components/ProductCard";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import Loading from "@/components/Loading";
+import { useAppContext } from "@/context/AppContext";
+import React from "react";
