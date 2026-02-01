0b7b2d6a0fa0f5f61b7b49ed87e1ecceff617f7a
raska26 <radityasandika51@gmail.com>
2026-02-01 07:23:57 +0700

    menambahkan pesan order berhasil

--- web/app/order-placed/page.jsx
+++ web/app/order-placed/page.jsx
@@ -20,6 +20,13 @@ return (
     <div className="flex justify-center items-center relative"></div>
     <Image className="absolute p-5" src={assets.checkmark} alt='' />
       <div className="animate-spin rounded-full h-24 w-24 border-4 border-t-green-300 border-gray-200"></div>
-      
+         </div>
+      <div className="text-center text-2xl font-semibold">Order Placed Successfully</div>
+    </div>
+  )
+}
+
+export default OrderPlaced
+
 
 
