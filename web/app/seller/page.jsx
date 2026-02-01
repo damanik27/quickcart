ded9233c84732b2f4220b8d99e13cbb382391c60
raska26 <radityasandika51@gmail.com>
2026-02-01 09:26:45 +0700

    tambah state dan handleSubmit di AddProduct

--- web/app/seller/page.jsx
+++ web/app/seller/page.jsx
@@ -2,3 +2,17 @@
 import React, { useState } from "react";
 import { assets } from "@/assets/assets";
 import Image from "next/image";
+
+const AddProduct = () => {
+
+  const [files, setFiles] = useState([]);
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [category, setCategory] = useState('Earphone');
+  const [price, setPrice] = useState('');
+  const [offerPrice, setOfferPrice] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+  };
